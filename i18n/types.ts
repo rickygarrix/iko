9a868ab2815db1f1d97a087decc87fd106243970
Nishimoto Hiroaki <129836027+rickygarrix@users.noreply.github.com>
2025-04-17 12:32:33 +0900

    Fix: remove old pages component & add i18n types

---
+++ i18n/types.ts
@@ -0,0 +1,13 @@
+// ✅ types.ts
+
+export type Messages = {
+  top: {
+    catchcopy: string;
+    search: string;
+    recommend: string;
+  };
+  meta: {
+    title: string;
+    description: string;
+  };
+};
