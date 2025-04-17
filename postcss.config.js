5ecdaba116de22ca9e9a24af1526b6b0a4ce1b5f
Nishimoto Hiroaki <129836027+rickygarrix@users.noreply.github.com>
2025-04-17 13:01:33 +0900

    chore: unify PostCSS config to single file

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+/** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: {
+    // Tailwind ⇢ autoprefixer の順に通す
     tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    autoprefixer: {}
+  }
+};
